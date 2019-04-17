8c339aa57b2097d7e3262b4dc60a8b59cd025411
Sean A Gleason <49649198+seaniemaster@users.noreply.github.com>
2019-04-17 16:13:41 -0400

    Add files via upload

---
+++ TestObserver-xom/src/com/ibm/demo/Request.java
@@ -0,0 +1,23 @@
+package com.ibm.demo;
+
+import java.util.Date;
+
+public class Request {
+
+	private Date date;
+	private int inputNumber;
+
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getInputNumber() {
+		return inputNumber;
+	}
+	public void setInputNumber(int inputNumber) {
+		this.inputNumber = inputNumber;
+	}
+}
